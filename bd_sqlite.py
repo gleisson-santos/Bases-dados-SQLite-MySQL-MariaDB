5f0f9330e1b7427ad73815f8e59ff564855b813c
Glfx20 <jsgleisson@gmail.com>
2023-06-04 16:50:17 -0300

    i

--- bd_sqlite.py
+++ bd_sqlite.py
@@ -3,34 +3,34 @@ import sqlite3
 conexao = sqlite3.connect('basededados.db')
 cursor = conexao.cursor()
 
-# cursor.execute('CREATE TABLE IF NOT EXISTS clientes ('
-#                 'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-#                 'nome TEXT,'
-#                 'peso REAL'
-#                 ')')
+cursor.execute('CREATE TABLE IF NOT EXISTS clientes ('
+               'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+               'nome TEXT,'
+               'peso REAL'
+               ')')
 
-#cursor.execute('INSERT INTO clientes (nome, peso) VALUES ("Gleisson", 80.5)')
+# cursor.execute('INSERT INTO clientes (nome, peso) VALUES ("Gleisson", 80.5)')
 
 # cursor.execute('INSERT INTO clientes (nome, peso) VALUES (?, ?)', ('Maria', 50))
 
 # cursor.execute(
-#     'INSERT INTO clientes (nome, peso) VALUES (:nome, :peso)', 
+#     'INSERT INTO clientes (nome, peso) VALUES (:nome, :peso)',
 #     {'nome': 'Joãozinho', 'peso': 25})
 
 # cursor.execute(
-#     'INSERT INTO clientes VALUES (:id, :nome, :peso)', 
+#     'INSERT INTO clientes VALUES (:id, :nome, :peso)',
 #     {'id': None, 'nome': 'Daniel', 'peso': 113})
 
 # conexao.commit()
 
-#ATUALIZANDO DADOS PELO ID
+# ATUALIZANDO DADOS PELO ID
 # cursor.execute(
 #     'UPDATE clientes SET nome=:nome WHERE id=:id',
 #     {'nome': 'Joana', 'id':6}
 # )
 # conexao.commit()
 
-#EXCLUINDO DADOS
+# EXCLUINDO DADOS
 # cursor.execute(
 #     'DELETE FROM clientes WHERE id=:id',
 #     {'id':6}
@@ -39,14 +39,14 @@ cursor = conexao.cursor()
 #
 
 
-
 cursor.execute('SELECT nome, peso FROM clientes WHERE peso > 50')
 
 for linha in cursor.fetchall():
     nome, peso = linha
 
-    print(nome, peso)
-    
+    # print(nome, peso)
+    print(linha)
+
 
 cursor.close()
 conexao.close()
